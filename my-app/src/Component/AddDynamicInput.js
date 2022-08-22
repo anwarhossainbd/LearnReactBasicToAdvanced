e0f572e2dfe570effc29d4881fb17e8065ec6dc3
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-08-22 21:16:55 +0600

    Basic form

--- my-app/src/Component/AddDynamicInput.js
+++ my-app/src/Component/AddDynamicInput.js
@@ -2,19 +2,32 @@ import React,{useState} from 'react'
 
 function AddDynamicInput() {
 
-    const [state, setState] = useState(0)
-
-    const incNumber=()=>{
-        setState(state+1)
-    }
    
+   const [name,setName] =useState("")
+      const [name2,setName2] =useState("")
+
+
+   const inputEvent=(e)=>{
+    setName(e.target.value)
+   }
+   
+
+   const clickHandele =()=>{
+    setName2(name)
+   }
+
+
+
 
   return (
     <div>
     
 
-    <h1>{state}</h1>
-    <button onClick={incNumber}>Click Me</button>
+    <h2>Hello {name} </h2>
+
+    <input type="text" placeholder="enter your name" value={name} onChange={inputEvent}  /> <br />
+    <button onClick={clickHandele}> Submit</button>
+    <p>{name2}</p>
     
     
     </div>
